758fb1734d7c20c523fac31a2e9e2c1b715301ca
rezarex <marvin.wekesa@gmail.com>
2023-01-15 22:22:24 +0300

    edits

--- tuples.py
+++ tuples.py
@@ -3,3 +3,12 @@ sample, not yet tested
 '''
 t = input(),
 print(hash(t))
+
+'''
+~~~~~somehow this code is not being accepted yet it is correct~~~~~
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+lst = map(int, input().split())
+t = tuple(lst)
+print(hash(t))
+#print(lst)
+'''
